7ade0a1c10e4ab4f7acc466833342a1ae7510371
le viet <leviet0115@gmail.com>
2022-03-07 11:10:10 +0200

    creating signature canvas

--- signature-app/src/App.js
+++ signature-app/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import SignaturePad from "react-signature-canvas";
+import "./App.css";
 
 function App() {
+  const sigPad = useRef({});
+  const [sig, setSig] = useState();
+
+  const clearSig = () => {
+    sigPad.current.clear();
+    sigPad.current.on();
+  };
+
+  const saveSig = () => {
+    setSig(sigPad.current.toDataURL()); //saving the PNG signature image as a base64 string
+    sigPad.current.off();
+    alert(
+      "Saved! Clear the canvas then check with the show last save function!"
+    );
+  };
+
+  const lastSig = () => {
+    if (sig === undefined) {
+      alert("No signature has been saved!");
+    } else {
+      clearSig();
+      sigPad.current.fromDataURL(sig);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="sigCanvas">
+        <SignaturePad ref={sigPad} canvasProps={{ className: "sigPad" }} />
+      </div>
+      <div>
+        <div className="buttonGroup">
+          <button onClick={clearSig}>Clear</button>
+          <button onClick={saveSig}>Save</button>
+          <button onClick={lastSig}>Show last save</button>
+        </div>
+      </div>
     </div>
   );
 }
