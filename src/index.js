d69a0d6cc07aa3ff02c5f6c3c899a3832133f192
anyajegorova <anna.jerm@outlook.com>
2022-03-13 20:17:54 +0200

    function to send data to back-end and upload files

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import FileUpload from './components/FileUpload';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <FileUpload />
   </React.StrictMode>,
   document.getElementById('root')
 );
