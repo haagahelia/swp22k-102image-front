522ac69dc20923ceb7bde59d914bb0b0bdf808ff
anyajegorova <anna.jerm@outlook.com>
2022-03-14 08:58:33 +0200

    sending data to back end

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import FileUpload from './components/FileUpload';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <FileUpload />
   </React.StrictMode>,
   document.getElementById('root')
 );
